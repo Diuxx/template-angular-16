f1fad9927a47067ae98efa95816b742b786863ec
Diuxx <nicolasmarmot@gmail.com>
2023-10-22 16:13:44 +0200

    feat(build): update build params

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@ app_name="template-angular-16"
 deliver_path="/var/www/nicoblog";
 repo="git@github.com:Diuxx/template-angular-16.git"
 branch="develop"
+nginx_conf="./nginx/nicoblog_nginx_conf"
 
 # Check if the directory already exists
 if [ ! -d "$deliver_path" ]; then
@@ -24,7 +25,14 @@ eval "npm i"
 eval "npm run build-prod"
 
 # Deploy
-eval "sudo cp -R ./dist/ ${deliver_path}"
+eval "sudo cp -R ./dist/* ${deliver_path}"
+
+# nginx
+eval "sudo cp -u ${nginx_conf} /etc/nginx/sites-available/"
+eval "sudo ln -s /etc/nginx/sites-available/nicoblog_nginx_conf /etc/nginx/sites-enabled/"
+
+eval "sudo nginx -t"
+eval "sudo systemctl reload nginx"
 
 # Finalize
 # echo "cleaning files..."
